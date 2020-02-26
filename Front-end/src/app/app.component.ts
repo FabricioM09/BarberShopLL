be239016937318aad97d910fa8a2ca16e1492185
Fabrimorales10 <fabricio.morales@delaguayaba.com>
2020-02-25 21:43:54 -0600

    Finish all sections

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 
 @Component({
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'BarberShopLL';
-s
+
+    
 }
--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { GalleryModule } from  '@ngx-gallery/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
+/* Custom Hammer configuration */
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import * as Hammer from 'hammerjs';
+
+export class CustomHammerConfig extends HammerGestureConfig {
+  overrides = {
+    'pan': {
+      direction: Hammer.DIRECTION_ALL,
+    }
+  }
+}
+/* End Custom hammer configuration */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    
   ],
   imports: [
     BrowserModule,
-   
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MDBBootstrapModule.forRoot(),
+    NgxGalleryModule,
+    GalleryModule
   ],
-  providers: [],
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [{provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
